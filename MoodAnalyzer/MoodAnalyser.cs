8b9db07ff34628e0f6015341e5b70a8207650649
Nishanth <rajkumarnishanth@gmail.com>
2022-03-07 00:21:36 +0530

    Passed both test cases given in PDF

--- MoodAnalyzer/MoodAnalyser.cs
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -5,7 +5,7 @@ public class MoodAnalyser
     public string AnalyseMood(string message)
     {
         if (message.Contains("Sad", StringComparison.OrdinalIgnoreCase) is true)
-            return "Sad Mood";
-        return "Happy Mood";
+            return "Sad";
+        return "Happy";
     }
 }
---
+++ MoodTest/TestMood.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace MoodTest;
+
+[TestClass]
+public class TestMood
+{
+    /// <summary>
+    /// Tests for sad mood.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    [TestMethod]
+    [DataRow("I am in Sad Mood")]
+    public void TestSadMood(string message)
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        string result = moodAnalyser.AnalyseMood(message);
+        Assert.AreEqual("Sad", result);
+    }
+
+    /// <summary>
+    /// Tests for happy mood.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    [TestMethod]
+    [DataRow("I am in any Mood")]
+    public void TestHappyMood(string message)
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        string result = moodAnalyser.AnalyseMood(message);
+        Assert.AreEqual("Happy", result);
+    }
+}
